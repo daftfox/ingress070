b86055f0b4f66235d1339150dcda24d12b172516
daftfox <tim.claassens@gmail.com>
2015-07-13 02:20:11 +0200

    Adjusted page layout for small screens

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,13 +8,18 @@
  * Controller of the fs070App
  */
 angular.module('ingress070App')
-  .controller('MainCtrl',['$scope', '$location', '$mdToast', function ($scope, $location, $mdToast) {
+  .controller('MainCtrl',['$scope', '$location', '$mdToast', '$document', '$animate', '$timeout',
+  	function ($scope, $location, $mdToast, $document, $animate, $timeout) {
     $scope.theme = "default";
+    $scope.hideFAB = false;
+    $scope.openFAB = false;
     $scope.alterTheme = function(theme){
     	$scope.theme = theme;
     }
     $scope.navigateTo = function(page){
-    	console.log(page);
+    	$timeout(function(){
+    		$scope.openFAB = false;
+    	}, 200);
     	$location.path(page);
     };
     $scope.toastPosition = {
@@ -29,14 +34,28 @@ angular.module('ingress070App')
       	.join(' ');
   	};
     $scope.banana = function(){
-    	var toast = $mdToast.simple()
-          .content('Banana!')
-          .action('Close')
-          .hideDelay(10000)
-          .highlightAction(false)
-          .position($scope.getToastPosition());
+		var toast = $mdToast.simple()
+	      .content('Banana!')
+	      .action('Close')
+	      .hideDelay(10000)
+	      .highlightAction(false)
+	      .position($scope.getToastPosition());
 	    $mdToast.show(toast).then(function() {
 	      console.log("Banana!");
 	    });
     };
+
+    $document.bind('scroll', function() {
+	  $scope.$apply(function() {
+	    if ($document.scrollTop() > 150) {
+	      var element = angular.element(".header-navigation");
+	      $animate.addClass(element, "fade");
+	      $animate.removeClass(element, "removefade");
+	    } else {
+	      var element = angular.element(".header-navigation");
+	      $animate.addClass(element, "removefade");
+	      $animate.removeClass(element, "fade");
+	    }
+	  });
+	});
   }]);
